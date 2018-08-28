41825303faaa6d9840bb08e06f4dfcaeeeaa0dc6
wuzhongwei <94050803@qq.com>
2018-08-28 13:50:57 +0800

    修复样式问题

--- examples/App.jsx
+++ examples/App.jsx
@@ -6,17 +6,15 @@ import { Marquee } from '../components';
 import '../components/styles/index.scss';
 import './App.scss';
 
-
 class App extends PureComponent {
   static childContextTypes = {
-    store: PropTypes.object
+    store: PropTypes.object,
   }
 
   render() {
-   
     return (
       <div className="form-wrap">
-        <Marquee direction="landscape" continuous={true} >
+        <Marquee direction="landscape">
           <div>依照保监要求，为了保障您的利益，请准确填写以下实名信息。本平台将通过保监会官方渠道验证您的资质，并庄重承诺对您所提供的个人及相关验证信息严格保密。</div>
         </Marquee>
       </div>
